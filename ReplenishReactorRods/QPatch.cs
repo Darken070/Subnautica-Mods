8a73830811d336f36499cab6e37e75ebaf716707
AlexejheroYTB <alexejhero@gmail.com>
2018-03-25 16:19:48 +0300

    updated qpatch

--- ReplenishReactorRods/QPatch.cs
+++ ReplenishReactorRods/QPatch.cs
@@ -9,9 +9,7 @@ namespace ReplenishReactorRods
         public static void Patch()
         {
             var RRR = TechTypePatcher.AddTechType("RRR", "Replenished Reactor Rod", "Used to power nuclear reactors. Cannot be replenished. Added by ReplenishReactorRods mod");
-            var URR = TechTypePatcher.AddTechType("URR", "Unreplenishable Reactor Rod", "Good for throwing away. Added by ReplenishReactorRods mod")
-            var RRRsprite = SpriteManager.Get(TechType.ReactorRod);
-            var URRsprite = SpriteManager.Get(TechType.DepletedReactorRod);
+            var URR = TechTypePatcher.AddTechType("URR", "Unreplenishable Reactor Rod", "Good for throwing away. Added by ReplenishReactorRods mod");
             var RRRcraft = new TechDataHelper
             {
                 _craftAmount = 1,
@@ -22,6 +20,10 @@ namespace ReplenishReactorRods
                 },
                 _techType = RRR
             };
+            var RRRsprite = SpriteManager.Get(TechType.ReactorRod);
+            var URRsprite = SpriteManager.Get(TechType.DepletedReactorRod);
+            CustomSpriteHandler.customSprites.Add(new CustomSprite(RRR, RRRsprite));
+            CustomSpriteHandler.customSprites.Add(new CustomSprite(URR, URRsprite));
             CraftDataPatcher.customTechData.Add(RRR, RRRcraft);
             CraftTreePatcher.customCraftNodes.Add("AlexejheroYTB's Mods/ReplenishReactorRods/RRR", RRR);
         }

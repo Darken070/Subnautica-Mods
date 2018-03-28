70aa3bb1e24face579348d3b5e9d1ba1894b8658
AlexejheroYTB <alexejhero@gmail.com>
2018-03-28 14:26:32 +0300

    changed '' to ""

--- MoreIngots/FabricatorScheme_Patch.cs
+++ MoreIngots/FabricatorScheme_Patch.cs
@@ -8,9 +8,10 @@ namespace MoreIngots.Patches
     {
         static bool Postfix(CraftNode result)
         {
-            result['Resources'].AddNode(new CraftNode[] { new CraftNode("MoreIngots", TreeAction.Expand, TechType.None) });
-            result['Resources']['MoreIngots'].AddNode(new CraftNode[] { new CraftNode("Craft", TreeAction.Expand, TechType.None) });
-            result['Resources']['MoreIngots'].AddNode(new CraftNode[] { new CraftNode("Unpack", TreeAction.Expand, TechType.None) });
+            result["Resources"].AddNode(new CraftNode[] { new CraftNode("MoreIngots", TreeAction.Expand, TechType.None) });
+            result["Resources"]["MoreIngots"].AddNode(new CraftNode[] { new CraftNode("Craft", TreeAction.Expand, TechType.None) });
+            result["Resources"]["MoreIngots"].AddNode(new CraftNode[] { new CraftNode("Unpack", TreeAction.Expand, TechType.None) });
+            return true;
         }
     }
 }

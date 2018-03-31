85bc4b37edc341e7eb435c92887b3d62e493ddbb
AlexejheroYTB <alexejhero@gmail.com>
2018-03-31 14:56:27 +0300

    added a new mod

---
+++ UnpackPlasteelIngots/QPatch.cs
@@ -0,0 +1,28 @@
+﻿using SMLHelper.Patchers;
+using System.Collections.Generic;
+
+namespace UnpackPlasteelIngots
+{
+    public class QPatch
+    {
+        public static void Patch()
+        {
+            var techData = new TechDataHelper
+            {
+                _craftAmount = 2,
+                _ingredients = new List<IngredientHelper>()
+                {
+                    new IngredientHelper(TechType.PlasteelIngot, 1)
+                },
+                _linkedItems = new List<TechType>()
+                {
+                    TechType.Titanium
+                },
+                _techType = TechType.Lithium
+            };
+            CraftDataPatcher.customTechData.Add(TechType.Lithium, techData);
+            CraftTreePatcher.customNodes.Add(new CustomCraftNode(TechType.Lithium, CraftScheme.Fabricator, "Resources/BasicResources"));
+            KnownTechPatcher.unlockedAtStart.Add(TechType.Lithium);
+        }
+    }
+}

ea606c8b13a6082c1d6489bdf4a877b21bb14c56
AlexejheroYTB <alexejhero@gmail.com>
2018-04-13 20:02:46 +0300

    new mod - customitemsizes

---
+++ CustomItemSizes/QPatch.cs
@@ -0,0 +1,28 @@
+﻿using SMLHelper;
+using SMLHelper.Patchers;
+using System.Collections.Generic;
+using UnityEngine;
+using Utilites.Config;
+using Logger = Utilites.Logger.Logger;
+using LogType = Utilites.Logger.LogType;
+using LogLevel = Utilites.Logger.LogLevel;
+using System;
+using Utilites.Logger;
+
+namespace CustomItemSizes
+{
+    public class QPatch
+    {
+        public static void Patch()
+        {
+            Load();
+        }
+        public static void Load()
+        {
+            foreach (TechType tt in Enum.GetValues(typeof(TechType)))
+            {
+
+            }
+        }
+    }
+}

98725c2cce69b2349062b293321e15ae9574f36a
AlexejheroYTB <alexejhero@gmail.com>
2018-04-22 19:17:06 +0300

    release 1.0.0 customitemsizes

--- CustomItemSizes/QPatch.cs
+++ CustomItemSizes/QPatch.cs
@@ -1,26 +1,35 @@
-﻿using SMLHelper;
-using SMLHelper.Patchers;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System;
 using Utilites.Config;
-using Logger = Utilites.Logger.Logger;
-using LogType = Utilites.Logger.LogType;
-using LogLevel = Utilites.Logger.LogLevel;
-using System;
 using Utilites.Logger;
+using SMLHelper.Patchers;
 
 namespace CustomItemSizes
 {
     public class QPatch
     {
+        public static readonly ConfigFile Config = new ConfigFile("config");
+
         public static void Patch()
         {
-            Load();
-        }
-        public static void Load()
-        {
-            foreach (TechType tt in Enum.GetValues(typeof(TechType)))
+            try
             {
+                Config.Load();
+                foreach (TechType techType in Enum.GetValues(typeof(TechType)))
+                {
+                    var name = techType.ToString();
+                    var x = CraftData.GetItemSize(techType).x;
+                    var y = CraftData.GetItemSize(techType).y;
+                    var configChanged =
+                      Config.TryGet(ref x, name, "X")
+                    | Config.TryGet(ref y, name, "Y");
+                    if (configChanged)
+                        Config.Save();
+                    CraftDataPatcher.customItemSizes[techType] = new Vector2int(x, y);
+                }
+            }
+            catch (Exception e)
+            {
+                e.Log(LogType.Console);
             }
         }
     }

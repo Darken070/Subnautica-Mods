6508157d1a8ceb73ec6d84d88337c12115293a95
AlexejheroYTB <alexejhero@gmail.com>
2018-05-06 16:57:43 +0300

    changed some assets

--- MoreHullPlates/Data/HullPlates/yenzgaming.cs
+++ MoreHullPlates/Data/HullPlates/yenzgaming.cs
@@ -11,7 +11,7 @@ namespace MoreHullPlates.HullPlates
             return GameObject.Get(yenztt, "Hullyenzgaming", yenztexture);
         }
 
-        private static TechType yenztt = TechTypePatcher.AddTechType("Hullyenzgaming", "yenzgaming Hull Plate", "Modder. Creator of Custom Food and Egg Info (Item added by MoreHullPlates)");
+        private static TechType yenztt = TechTypePatcher.AddTechType("Hullyenzgaming", "Yenzgaming Hull Plate", "Modder. Creator of Custom Food and Egg Info (Item added by MoreHullPlates)");
 
         private static Texture2D yenztexture = 
             AssetBundle.LoadFromFile($@"./QMods/MoreHullPlates/Assets/yenzgaming.assets").

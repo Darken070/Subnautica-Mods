183e08aee7453f9bbd3c0aa7444b23ec2e3dad43
AlexejheroYTB <alexejhero@gmail.com>
2018-05-12 16:16:02 +0300

    using sprite

--- MoreHullPlates/Data/Items/Custom/AlexejheroYTB.cs
+++ MoreHullPlates/Data/Items/Custom/AlexejheroYTB.cs
@@ -16,12 +16,13 @@ namespace MoreHullPlates.Items
             }
             private static TechType alexejheroytbtt = TechTypePatcher.AddTechType("HullAlexejheroYTB", "AlexejheroYTB Hull Plate", "Modder. Creator of MoreIngots and MoreHullPlates (Item added by MoreHullPlates)");
             private static Texture2D texture = AssetBundle.LoadFromFile($@"./QMods/MoreHullPlates/Assets/alexejheroytb.assets").LoadAsset<UnityEngine.Sprite>("AlexejheroYTB").texture;
+            private static UnityEngine.Sprite sprite = AssetBundle.LoadFromFile($@"./QMods/MoreHullPlates/Assets/icon.assets").LoadAsset<UnityEngine.Sprite>("bikemanhullplate");
             public static void AlexejheroYTB()
             {
                 CraftDataPatcher.customBuildables.Add(alexejheroytbtt);
                 CraftDataPatcher.AddToCustomGroup(TechGroup.Miscellaneous, TechCategory.MiscHullplates, alexejheroytbtt);
                 CustomPrefabHandler.customPrefabs.Add(new CustomPrefab("HullAlexejheroYTB", "Submarine/Build/HullAlexejheroYTB", alexejheroytbtt, AYTBGameObject));
-                CustomSpriteHandler.customSprites.Add(new CustomSprite(alexejheroytbtt, Sprite.Get));
+                CustomSpriteHandler.customSprites.Add(new CustomSprite(alexejheroytbtt, sprite));
                 CraftDataPatcher.customTechData.Add(alexejheroytbtt, TechData.Get);
             }
         }

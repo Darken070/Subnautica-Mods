87b9a2e7fd99ab5e3c1238f48fcd825082a7d8da
AlexejheroYTB <alexejhero@gmail.com>
2018-05-16 19:43:23 +0300

    some tweaks

--- MoreHullPlates/Main.cs
+++ MoreHullPlates/Main.cs
@@ -2,8 +2,9 @@
 using SMLHelper.Patchers;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
-using Utilites.Logger;
+using static MoreHullPlates.Code;
 
 namespace MoreHullPlates
 {
@@ -22,94 +23,65 @@ namespace MoreHullPlates
             }
         }
     }
-    public class AssetBundleLocation
+    public class Code
     {
-        public string AssetBundle;
-        public string Path;
-
-        public AssetBundleLocation(string assetBundle, string path)
+        public class AssetBundleLocation
         {
-            try
+            public string AssetBundle;
+            public string Path;
+
+            public AssetBundleLocation(string assetBundle, string path)
             {
-                AssetBundle = assetBundle.ToLower();
-                Path = path;
+                try
+                {
+                    AssetBundle = assetBundle.ToLower();
+                    Path = path;
+                }
+                catch (Exception e)
+                {
+                    e.Log();
+                }
             }
-            catch (Exception e)
+
+            public AssetBundleLocation(string assetBundle_And_Path_If_They_Are_The_Same_But_Keep_Capitalization)
             {
-                e.Log();
+                try
+                {
+                    AssetBundle = assetBundle_And_Path_If_They_Are_The_Same_But_Keep_Capitalization.ToLower();
+                    Path = assetBundle_And_Path_If_They_Are_The_Same_But_Keep_Capitalization;
+
+                }
+                catch (Exception e)
+                {
+                    e.Log();
+                }
             }
         }
-
-        public AssetBundleLocation(string assetBundle_And_Path_If_They_Are_The_Same_But_Keep_Capitalization)
+        public class ItemToUnlock
         {
-            try
-            {
-                AssetBundle = assetBundle_And_Path_If_They_Are_The_Same_But_Keep_Capitalization.ToLower();
-                Path = assetBundle_And_Path_If_They_Are_The_Same_But_Keep_Capitalization;
+            public TechType TechType;
+            public string Name;
+            public string Tooltip;
+            public bool RemoveFromOtherCustomGroup = false;
 
-            }
-            catch (Exception e)
+            public ItemToUnlock(TechType techType, string name, string tooltip, bool removeFromOtherCustomGroup = false)
             {
-                e.Log();
+                try
+                {
+                    TechType = techType;
+                    Name = name;
+                    Tooltip = tooltip;
+                    RemoveFromOtherCustomGroup = removeFromOtherCustomGroup;
+                }
+                catch (Exception e)
+                {
+                    e.Log();
+                }
             }
         }
-    }
-    public class AmbiguousSprite
-    {
-        public Atlas.Sprite AtlasSprite;
-        public Sprite UnitySprite;
-
-        public AmbiguousSprite(Atlas.Sprite _atlasSprite)
+        public static class ItemsToUnlockList
         {
-            try
-            {
-                AtlasSprite = _atlasSprite;
-                UnitySprite = null;
-            }
-            catch (Exception e)
-            {
-                e.Log();
-            }
-        }
-
-        public AmbiguousSprite(Sprite _unitySprite)
-        {
-            try
-            {
-                AtlasSprite = null;
-                UnitySprite = _unitySprite;
-            }
-            catch (Exception e)
-            {
-                e.Log();
-            }
-        }
-    }
-    public class ItemToUnlock
-    {
-        public TechType TechType;
-        public string Name;
-        public string Tooltip;
-        public bool RemoveFromOtherCustomGroup = false;
-
-        public ItemToUnlock(TechType techType, string name, string tooltip, bool removeFromOtherCustomGroup = false)
-        {
-            try
-            {
-            TechType = techType;
-            Name = name;
-            Tooltip = tooltip;
-            RemoveFromOtherCustomGroup = removeFromOtherCustomGroup;
-            }
-            catch (Exception e)
-            {
-                e.Log();
-            }
-        }
-    }
-    public static class ItemsToUnlockList
-    {
-        public static List<ItemToUnlock> itemsToUnlock = new List<ItemToUnlock>()
+            public static List<ItemToUnlock> itemsToUnlock = new List<ItemToUnlock>()
         {
             new ItemToUnlock(TechType.DevTestItem, "Charlie Cleveland Hull Plate", "Developer at UnknownWorlds. One of Subnautica's Devs"),
             new ItemToUnlock(TechType.SpecialHullPlate, "Special Edition Hull Plate", "Limited edition numbered hull plate"),
@@ -127,34 +99,34 @@ namespace MoreHullPlates
             new ItemToUnlock(TechType.JackSepticEye, "jacksepticeye's Tank", "Subnautica content creator. https://www.youtube.com/jacksepticeye", true),
             new ItemToUnlock(TechType.EatMyDiction, "Diction's Cat", "Subnautica content creator. https://www.youtube.com/user/EatMyDiction1")
         };
-    }
-    public class ItemToAdd
-    {
-        public string InternalName;
-        public string Name;
-        public string Tooltip;
-        public AssetBundleLocation TextureLocation;
-        public AssetBundleLocation SpriteLocation;
-
-        public ItemToAdd(string internalName, string name, string tooltip, AssetBundleLocation textureLocation, AssetBundleLocation spriteLocation)
+        }
+        public class ItemToAdd
         {
-            try
+            public string InternalName;
+            public string Name;
+            public string Tooltip;
+            public AssetBundleLocation TextureLocation;
+            public AssetBundleLocation SpriteLocation;
+
+            public ItemToAdd(string internalName, string name, string tooltip, AssetBundleLocation textureLocation, AssetBundleLocation spriteLocation)
             {
-            InternalName = internalName;
-            Name = name;
-            Tooltip = tooltip;
-            TextureLocation = textureLocation;
-            SpriteLocation = spriteLocation;
-            }
-            catch (Exception e)
-            {
-                e.Log();
+                try
+                {
+                    InternalName = internalName;
+                    Name = name;
+                    Tooltip = tooltip;
+                    TextureLocation = textureLocation;
+                    SpriteLocation = spriteLocation;
+                }
+                catch (Exception e)
+                {
+                    e.Log();
+                }
             }
         }
-    }
-    public static class ItemsToAddList
-    {
-        public static List<ItemToAdd> itemsToAdd = new List<ItemToAdd>()
+        public static class ItemsToAddList
+        {
+            public static List<ItemToAdd> itemsToAdd = new List<ItemToAdd>()
         {
             new ItemToAdd("HullAlexejheroYTB", "AlexejheroYTB Hull Plate", "Modder. Creator of MoreIngots and MoreHullPlates", new AssetBundleLocation("AlexejheroYTB"), null),
             new ItemToAdd("HullAHK1221", "AHK1221 Hull Plate", "Modder. Creator of Modding Helper, Warp Cannon, Exterior Plant Pots and much more", new AssetBundleLocation("AHK1221"), null),
@@ -166,140 +138,167 @@ namespace MoreHullPlates
             new ItemToAdd("HullVlad00003", "Vlad-00003 Hull Plate", "Modder. Creator of Utilities and Drillable Scan", new AssetBundleLocation("vlad00003", "Vlad-00003"), null),
             new ItemToAdd("Hullyenzgaming", "yenzgaming Hull Plate", "Modder. Creator of Custom Food and Egg Info", new AssetBundleLocation("yenzgaming"), new AssetBundleLocation("yenzgaming-icon", "yenzgaming Icon"))
         };
-    }
-    public class TechData
-    {
-        public static TechDataHelper Get = new TechDataHelper
+        }
+        public class TechData
         {
-            _craftAmount = 1,
-            _ingredients = new List<IngredientHelper>()
+            public static TechDataHelper Get = new TechDataHelper
+            {
+                _craftAmount = 1,
+                _ingredients = new List<IngredientHelper>()
                 {
                     new IngredientHelper(TechType.Titanium, 1),
                     new IngredientHelper(TechType.Glass, 1)
                 }
-        };
-    }
-    public class DefaultSprite
-    {
-        public static Atlas.Sprite Get = SpriteManager.Get(TechType.SpecialHullPlate);
-    }
-    public class DefaultPrefab
-    {
-        public static GameObject Get(TechType techtype, string Classid, Texture2D texture)
+            };
+        }
+        public class DefaultSprite
         {
-            try
+            public static Atlas.Sprite Get = SpriteManager.Get(TechType.SpecialHullPlate);
+        }
+        public class DefaultPrefab
+        {
+            public static GameObject Get(TechType techtype, string Classid, Texture2D texture)
             {
-                var prefab = Resources.Load<GameObject>("Submarine/Build/IGPHullPlate");
-                var obj = UnityEngine.Object.Instantiate(prefab);
-                var child = obj.transform.Find("Icon").gameObject;
+                try
+                {
+                    var prefab = Resources.Load<GameObject>("Submarine/Build/IGPHullPlate");
+                    var obj = UnityEngine.Object.Instantiate(prefab);
+                    var child = obj.transform.Find("Icon").gameObject;
 
-                var constructable = obj.GetComponent<Constructable>();
-                var techTag = obj.GetComponent<TechTag>();
-                var prefabIdentifier = obj.GetComponent<PrefabIdentifier>();
-                var mesh = child.GetComponent<MeshRenderer>();
+                    var constructable = obj.GetComponent<Constructable>();
+                    var techTag = obj.GetComponent<TechTag>();
+                    var prefabIdentifier = obj.GetComponent<PrefabIdentifier>();
+                    var mesh = child.GetComponent<MeshRenderer>();
 
-                constructable.techType = techtype;
+                    constructable.techType = techtype;
 
-                techTag.type = techtype;
+                    techTag.type = techtype;
 
-                prefabIdentifier.ClassId = Classid;
+                    prefabIdentifier.ClassId = Classid;
 
-                mesh.material.mainTexture = texture;
+                    mesh.material.mainTexture = texture;
 
-                return obj;
-            }
-            catch (Exception e)
-            {
-                e.Log();
-                return null;
+                    return obj;
+                }
+                catch (Exception e)
+                {
+                    e.Log();
+                    return null;
+                }
             }
         }
-    }
-    public class ExistingItems
-    {
-        public static void Load()
+        public class ExistingItems
         {
-            try
+            public static void Load()
             {
-            foreach (ItemToUnlock item in ItemsToUnlockList.itemsToUnlock)
-            {
-                var _techType = item.TechType;
-                var _name = item.Name;
-                var _tooltip = item.Tooltip;
-                var _removeFromOtherCustomGroup = item.RemoveFromOtherCustomGroup;
-
-                KnownTechPatcher.unlockedAtStart.Add(_techType);
-                LanguagePatcher.customLines.Add(_techType.ToString(), _name);
-                LanguagePatcher.customLines.Add($"Tooltip_{_techType.ToString()}", $"{_tooltip} (Item added by Subnautica)");
-
-                if (_removeFromOtherCustomGroup)
+                try
                 {
-                    CraftDataPatcher.AddToCustomGroup(TechGroup.Miscellaneous, TechCategory.Misc, _techType);
-                    try
+                    foreach (ItemToUnlock item in ItemsToUnlockList.itemsToUnlock)
                     {
-                        CraftDataPatcher.RemoveFromCustomGroup(TechGroup.Miscellaneous, TechCategory.MiscHullplates, _techType);
-                    }
-                    catch
-                    {
-                        Console.WriteLine("[MoreHullPlates] [Error] PLEASE UPDATE MODDING HELPER TO VERSION 1.5.2");
+                        var _techType = item.TechType;
+                        var _name = item.Name;
+                        var _tooltip = item.Tooltip;
+                        var _removeFromOtherCustomGroup = item.RemoveFromOtherCustomGroup;
+
+                        KnownTechPatcher.unlockedAtStart.Add(_techType);
+                        LanguagePatcher.customLines.Add(_techType.ToString(), _name);
+                        LanguagePatcher.customLines.Add($"Tooltip_{_techType.ToString()}", $"{_tooltip} (Item added by Subnautica)");
+
+                        if (_removeFromOtherCustomGroup)
+                        {
+                            CraftDataPatcher.AddToCustomGroup(TechGroup.Miscellaneous, TechCategory.Misc, _techType);
+                            try
+                            {
+                                CraftDataPatcher.RemoveFromCustomGroup(TechGroup.Miscellaneous, TechCategory.MiscHullplates, _techType);
+                            }
+                            catch
+                            {
+                                Console.WriteLine("[MoreHullPlates] [Error] PLEASE UPDATE MODDING HELPER TO VERSION 1.5.2");
+                            }
+                        }
                     }
                 }
+                catch (Exception e)
+                {
+                    e.Log();
+                }
             }
-            }
-            catch (Exception e)
+        }
+        public class CustomItems
+        {
+            public static TechType ItemTechType { get; set; }
+            public static Texture2D ItemTexture { get; set; }
+            public static Sprite UnitySprite { get; set; }
+            public static string ItemInternalName { get; set; }
+            public static void Load()
             {
-                e.Log();
+                try
+                {
+                    foreach (ItemToAdd item in ItemsToAddList.itemsToAdd)
+                    {
+                        ItemInternalName = item.InternalName;
+                        var _name = item.Name;
+                        var _tooltip = item.Tooltip;
+                        var _textureLocation = item.TextureLocation;
+                        var _spriteLocation = item.SpriteLocation;
+
+                        ItemTechType = TechTypePatcher.AddTechType(ItemInternalName, _name, _tooltip, true);
+                        ItemTexture = AssetBundle.LoadFromFile($@"./QMods/MoreHullPlates/Assets/{_textureLocation.AssetBundle}.assets").LoadAsset<Sprite>(_textureLocation.Path).texture;
+                        if (_spriteLocation == null)
+                        {
+                            UnitySprite = null;
+                        }
+                        else
+                        {
+                            UnitySprite = AssetBundle.LoadFromFile($@"./QMods/MoreHullPlates/Assets/{_spriteLocation.AssetBundle}.assets").LoadAsset<Sprite>(_spriteLocation.Path);
+                        }
+                        CraftDataPatcher.customBuildables.Add(ItemTechType);
+                        CraftDataPatcher.AddToCustomGroup(TechGroup.Miscellaneous, TechCategory.MiscHullplates, ItemTechType);
+                        CustomPrefabHandler.customPrefabs.Add(new CustomPrefab(ItemInternalName, $"Submarine/Build/{ItemInternalName}", ItemTechType, ItemPrefab));
+                        if (UnitySprite == null)
+                        {
+                            CustomSpriteHandler.customSprites.Add(new CustomSprite(ItemTechType, DefaultSprite.Get));
+                        }
+                        else
+                        {
+                            CustomSpriteHandler.customSprites.Add(new CustomSprite(ItemTechType, UnitySprite));
+                        }
+                        CraftDataPatcher.customTechData.Add(ItemTechType, TechData.Get);
+                    }
+                }
+                catch (Exception e)
+                {
+                    e.Log();
+                }
+            }
+            public static GameObject ItemPrefab()
+            {
+                return DefaultPrefab.Get(ItemTechType, ItemInternalName, ItemTexture);
             }
         }
     }
-    public class CustomItems
+    public class Utility
     {
-        public static TechType ItemTechType { get; set; }
-        public static Texture2D ItemTexture { get; set; }
-        public static AmbiguousSprite ItemSprite { get; set; }
-        public static string ItemInternalName { get; set; }
-        public static void Load()
+        public class Logging
         {
-            try
+            public static string FormatException(Exception e)
             {
-            foreach (ItemToAdd item in ItemsToAddList.itemsToAdd)
-            {
-                Console.WriteLine("before var setting");
-                ItemInternalName = item.InternalName;
-                var _name = item.Name;
-                var _tooltip = item.Tooltip;
-                var _textureLocation = item.TextureLocation;
-                var _spriteLocation = item.SpriteLocation;
-
-                Console.WriteLine("before techtype patching");
-                ItemTechType = TechTypePatcher.AddTechType(ItemInternalName, _name, _tooltip, true);
-                Console.WriteLine("before texture patching");
-                ItemTexture = AssetBundle.LoadFromFile($@"./QMods/MoreHullPlates/Assets/{_textureLocation.AssetBundle}.assets").LoadAsset<Sprite>(_textureLocation.Path).texture;
-                Console.WriteLine("before sprite patching");
-                if (_spriteLocation == null)
-                {
-                    ItemSprite.AtlasSprite = new AmbiguousSprite(SpriteManager.Get(TechType.SpecialHullPlate)).AtlasSprite;
-                }
-                else
-                {
-                    ItemSprite.UnitySprite = new AmbiguousSprite(AssetBundle.LoadFromFile($@"./QMods/MoreHullPlates/Assets/{_spriteLocation.AssetBundle}.assets").LoadAsset<Sprite>(_spriteLocation.Path)).UnitySprite;
-                }
-                Console.WriteLine("before everything else");
-                CraftDataPatcher.customBuildables.Add(ItemTechType);
-                CraftDataPatcher.AddToCustomGroup(TechGroup.Miscellaneous, TechCategory.MiscHullplates, ItemTechType);
-                CustomPrefabHandler.customPrefabs.Add(new CustomPrefab(ItemInternalName, $"Submarine/Build/{ItemInternalName}", ItemTechType, ItemPrefab));
-                CustomSpriteHandler.customSprites.Add(new CustomSprite(ItemTechType, DefaultSprite.Get));
-                CraftDataPatcher.customTechData.Add(ItemTechType, TechData.Get);
+                if (e == null)
+                    return string.Empty;
+                return $"\"Exception: {e.GetType()}\"\n\tMessage: {e.Message}\n\tStacktrace: {e.StackTrace}\n" +
+                       FormatException(e.InnerException);
             }
-            }
-            catch (Exception e)
+            public static void Log(string text, string caller)
             {
-                e.Log();
+                Console.WriteLine($"[{caller}] {text}");
             }
         }
-        public static GameObject ItemPrefab()
+    }
+    public static class ExtensionMethods
+    {   
+        public static void Log(this Exception e)
         {
-            return DefaultPrefab.Get(ItemTechType, ItemInternalName, ItemTexture);
+            Utility.Logging.Log(Utility.Logging.FormatException(e), Assembly.GetCallingAssembly().GetName().Name);
         }
     }
 }

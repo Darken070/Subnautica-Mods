b48ed0ea39fd0888340b86d6398b4b98f7b7c8c7
AlexejheroYTB <alexejhero@gmail.com>
2018-07-23 20:42:33 +0300

    Added InfiniteOxygen project

---
+++ InfiniteOxygen/Class1.cs
@@ -0,0 +1,25 @@
+﻿using Harmony;
+using System.Reflection;
+using UnityEngine;
+
+namespace InfiniteOxygen
+{
+    public static class Main
+    {
+        public static void Patch()
+        {
+            HarmonyInstance.Create("alexejheroytb.subnauticamods.infiniteoxygen").PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    [HarmonyPatch(typeof(OxygenManager))]
+    [HarmonyPatch("Update")]
+    public static class OxygenManager_Update
+    {
+        [HarmonyPostfix]
+        public static void Postfix(OxygenManager __instance)
+        {
+            __instance.AddOxygen(Time.deltaTime);
+        }
+    }
+}
